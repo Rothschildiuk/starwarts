f48d49a707e90a4bc234d5a15235adfdbf813eb0
Oleh Baidiuk <oleh12@gmail.com>
2019-03-28 02:41:15 +0100

    add delete button

--- src/Components/Plot.js
+++ src/Components/Plot.js
@@ -1,13 +1,27 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import {GlobalContext} from '../App'
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const Plot = ({title, description}) => {
-
+    const {context, setContext} = useContext(GlobalContext)
 
     return <div className='Plot'>
         <p> type : {title}</p>
+        <p> description: {description}
+        <IconButton aria-label="Delete" onClick={() => {
+            let newContecst = [];
+            context.map(e => {
+                if (e.name !== description)
+                    newContecst.push(e)
 
-        <p> description: {description}</p>
+            })
+
+            setContext([...newContecst])
+        }}>
+            <DeleteIcon fontSize="small"/>
+        </IconButton>
+        </p>
     </div>
 
 
